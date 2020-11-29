3b13eb7c238f1f7c036b29391409a2127896f2b8
Vinh <nxvinh222@gmail.com>
2020-11-29 23:16:59 +0700

    Fix: banner picture

--- Sprint-2/xpharmacy-fe/src/components/Banner.js
+++ Sprint-2/xpharmacy-fe/src/components/Banner.js
@@ -3,7 +3,7 @@ import 'styles/Banner.css'
 
 
 const logoUri =
-  "https://www.vodafone.com/business/media/image/1508862781296/640/251121730-h_800x400.jpg";
+  "https://i.pinimg.com/originals/8a/17/3a/8a173a60fe0a3a49787b21ad625d7633.jpg";
 
 // const carouselPicture =
 //     "https://upload.wikimedia.org/wikipedia/commons/5/52/Golden_sky_%284080648531%29.jpg"
@@ -14,10 +14,10 @@ function Banner(props) {
   return (
       <div className="hero-banner">
          <div className="hero-text">         
-          <img className="col-12 p-0" src={logoUri} style={{minHeight: "275px", height: "50%",   objectFit: "cover"}}  alt="hero banner"/>
-          <div className="hero-text-top text-center">
+          <img className="bg col-12 p-0" src={logoUri} style={{minHeight: "275px", objectFit: "cover"}}  alt="hero banner"/>
+          {/* <div className="hero-text-top text-center">
               <h1 className="display-3 text-center" id="hero-text-detail" >{props.bannerText}</h1>
-          </div>
+          </div> */}
         </div>
       </div>
 
